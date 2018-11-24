f1045b93ea6c82a56db1d8d5642c7e36db85ccd1
putrizarkhmwt <putrizarkhmwt@gitlab.com>
2018-11-24 14:47:40 +0700

    tampilan edit profil

--- app/src/main/java/com/example/lenovo/itaninew/EditProfil.java
+++ app/src/main/java/com/example/lenovo/itaninew/EditProfil.java
@@ -4,14 +4,58 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import com.example.lenovo.itaninew.KUR.FormKUR2;
 
 public class EditProfil extends AppCompatActivity {
+
+    String jk[]={"Laki-Laki","Perempuan"};
+    String prov[]={"Jawa Timur","Jawa Tengah", "Jawa Barat","Nusa Tenggara Barat", "Nusa Tenggara Timur"};
+    String kota[]={"Surabaya","Gresik", "Malang","Mojokerto", "Jombang","Kediri"};
+    String kec[]={"Sidayu","Dukun", "Panceng","Ujung Pangkah"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.itani_profil);
+
+        Spinner combo_jk = (Spinner) findViewById(R.id.combo_jk);
+        Spinner combo_provinsi = (Spinner) findViewById(R.id.combo_provinsi);
+        Spinner combo_kota = (Spinner) findViewById(R.id.combo_kota);
+        Spinner combo_kecamatan = (Spinner) findViewById(R.id.combo_kecamatan);
+
+        ArrayAdapter<String> AdapterListJK = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,jk);
+        AdapterListJK.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        combo_jk.setAdapter(AdapterListJK);
+
+        ArrayAdapter<String> AdapterListProv = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,prov);
+        AdapterListProv.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        combo_provinsi.setAdapter(AdapterListProv);
+
+        ArrayAdapter<String> AdapterListKota = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,kota);
+        AdapterListKota.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        combo_kota.setAdapter(AdapterListKota);
+
+        ArrayAdapter<String> AdapterListKecamatan = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,kec);
+        AdapterListKecamatan.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        combo_kecamatan.setAdapter(AdapterListKecamatan);
+
+        ImageView simpan = (ImageView) findViewById(R.id.simpan);
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simpan();
+            }
+        });
+    }
+
+    public void simpan(){
+        Intent next = new Intent(this, Home.class);
+        startActivity(next);
     }
 
 }
--- app/src/main/java/com/example/lenovo/itaninew/Home.java
+++ app/src/main/java/com/example/lenovo/itaninew/Home.java
@@ -1,10 +1,17 @@
 package com.example.lenovo.itaninew;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.example.lenovo.itaninew.KUR.FormKUR;
 import com.example.lenovo.itaninew.KUR.Persyaratan1;
@@ -19,7 +26,7 @@ public class Home extends AppCompatActivity {
 
         CardView cv_kur = (CardView) findViewById(R.id.navigation_kur);
         CardView cv_komoditas = (CardView) findViewById(R.id.navigation_komoditas);
-        CardView cv_support = (CardView) findViewById(R.id.navigation_support);
+
 
         cv_kur.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,12 +42,49 @@ public class Home extends AppCompatActivity {
             }
         });
 
-        cv_support.setOnClickListener(new View.OnClickListener() {
+        final ImageView pengaturan = (ImageView) findViewById(R.id.setting);
+        pengaturan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //activity_support();
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(Home.this, pengaturan);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.navigation, popup.getMenu());
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if(item.getTitle().equals("Edit Profil")) {
+                            Toast.makeText(Home.this, "You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                            profil();
+                        }else {
+                            Toast.makeText(Home.this, "You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                            keluar();
+                        }
+                        return true;
+                    }
+                });
+                popup.show();//showing popup menu
             }
-        });
+        });//closing the setOnClickListener method
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.profil){
+            Toast.makeText(getApplicationContext(),"You Click "+item.getTitle(),Toast.LENGTH_LONG).show();
+        } else if (item.getItemId() == R.id.keluar) {
+            Toast.makeText(getApplicationContext(),"You Click "+item.getTitle(),Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 
   public void activity_kur() {
@@ -53,6 +97,15 @@ public class Home extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void profil() {
+        Intent intent1 = new Intent(this, EditProfil.class);
+        startActivity(intent1);
+    }
+
+    public void keluar() {
+        Intent intent2 = new Intent(this, Login.class);
+        startActivity(intent2);
+    }
 
 
 }
