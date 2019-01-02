de50c276bf99329f88dd77ccfeef699a85eaca16
putrizarkhmwt <putrizarkhmwt@gitlab.com>
2019-01-02 18:02:45 +0700

    Tampilan Android Final

---
+++ app/src/main/java/com/example/lenovo/itaninew/VerifikasiKode.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.itaninew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifikasiKode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.itani_verifikasi_kode);
+    }
+}
